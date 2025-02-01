f59a03d7ec522049c88e066ace6c8228448b88c2
Charyll <charllycbncl@gmail.com>
2025-02-01 14:25:29 +0800

    initial

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,17 @@ import { SiteConfig } from "@/types";
 import { env } from "@/env.mjs";
 
 export const siteConfig: SiteConfig = {
-  name: "Karbi",
-  author: "Kurt Carvey Cadenas",
+  name: "Charyll",
+  author: "Charyll Mae Cabungcal",
   description: "My personal website built in Nextjs.",
   url: env.NEXT_PUBLIC_APP_URL,
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/logo.png`,
   links: {
-    github: "https://github.com/krtcrvy",
-    facebook: "https://facebook.com/krtcrvy",
-    twitter: "https://x.com/krtcrvy",
-    linkedin: "https://www.linkedin.com/in/kurtcarvey-cadenas",
-    email: "kurtcarvey.m.cadenas@gmail.com",
+    github: "https://github.com/asdfghjk000/CHARM",
+    facebook: "https://www.facebook.com/charyllmae.cabungcal.3",
+    twitter: "https://x.com/charyll",
+    linkedin: "https://www.linkedin.com/in/charyll-cabungcal",
+    email: "charllycbncl@gmail.com",
     resume:
       "https://drive.google.com/file/d/16OQGu47vpXpTBhVnehUiLKlgSsWLFN-i/view?usp=sharing",
   },
